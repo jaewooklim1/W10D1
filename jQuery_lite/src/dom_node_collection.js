fd7f037dbb4a91a023db58454a2fe824cfef7a50
jaewooklim1 <jaewooklim1@gmail.com>
2021-04-26 14:25:05 -0400

    finished removeclass, addclass, and attr

--- jQuery_lite/src/dom_node_collection.js
+++ jQuery_lite/src/dom_node_collection.js
@@ -26,24 +26,79 @@ class DOMNodeCollection {
         ele.innerHTML += arg;
       }) 
     } else if(arg instanceof HTMLElement) {
-      debugger
+      // debugger
       this.arr.forEach(function(ele) {
-        // ele.appendChild(arg);
-        ele.innerHTML += arg
-        // ele.each((child) => Node.appendChild(arg))
-        debugger
+        let clone = arg.cloneNode(true);
+        ele.appendChild(clone);
+   
+        // debugger
         // arg.outerHTML = ele.innerHTML;
       }) 
     } else {
       this.arr.forEach(function(ele1) {
-        arg.arr.forEach(function(ele2){
-          ele2.outerHTML = ele1.innerHTML;
+
+        if (Array.isArray(arg.arr)) {
+          arg.arr.forEach(function(ele2){
+            let clone = ele2.cloneNode(true);
+            ele1.appendChild(clone);        
         })
+        } else {  
+          // debugger
+          let clone = arg.arr.cloneNode(true);
+          ele1.appendChild(clone);
+        }
       }) 
     }
   }
 
+  attr(attributeName, value) {
+   if (value) {
+     this.arr.forEach(function(ele){
+       ele.setAttribute(attributeName, value);
+     })
+   } else {
+     return this.arr[0].getAttribute(attributeName);
+   }
+    
+  }
 
+  addClass(className) {
+    if (typeof (className) === "string") {
+      let classNames = className.split(" ");
+      this.arr.forEach(function(ele){
+        // ele.classList.add(classNames);
+        classNames.forEach(function(name){
+          ele.classList.add(name);
+        })
+      })
+    } else if (Array.isArray(className)) {
+      this.arr.forEach(function(ele) {
+        // ele.classList.add(className);
+        className.forEach(function(name){
+          ele.classList.add(name);
+        })
+      })
+    }
+  }
+
+  removeClass(className) {
+    if (typeof (className) === "string") {
+      let classNames = className.split(" ");
+      this.arr.forEach(function(ele){
+        // ele.classList.remove(classNames);
+        classNames.forEach(function(name){
+          ele.classList.remove(name);
+        })
+      })
+    } else if (Array.isArray(className)) {
+      this.arr.forEach(function(ele) {
+        // ele.classList.remove(className);
+        className.forEach(function(name){
+          ele.classList.remove(name);
+        })
+      })    
+    }
+  }
 }
 
 module.exports = DOMNodeCollection;
